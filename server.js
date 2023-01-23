ebba3a27cd2c6535957b261cfef2766065fd7697
Khoi Hoang <khoii87@gmail.com>
2023-01-23 21:43:33 +0100

    02 - Intro to Express JS Framework

--- server.js
+++ server.js
@@ -1,101 +1,55 @@
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-
-// initialize object
-class Emitter extends EventEmitter {}
-
-// add listener for the log event
-const emitter = new Emitter();
-emitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-	try {
-		const rawData = await fsPromises.readFile(filePath, !contentType.includes('image') ? 'utf8' : '');
-		const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+const app = express();
 
-		response.writeHead(filePath.includes('404.html') ? 404 : 200, { 'Content-Type': contentType });
-
-		response.end(contentType === 'application/json' ? JSON.stringify(data) : data);
-	} catch (error) {
-		console.log(error);
-		emitter.emit('log', `${error.name}: ${error.message}`, `errLog.txt`);
-
-		// cannot read the data from the server that we want
-		response.statusCode = 500;
-		response.end();
-	}
-};
-
-const server = http.createServer((req, res) => {
-	console.log(req.url, req.method);
-	emitter.emit('log', `${req.url}\t${req.method}`, `reqLog.txt`);
-
-	const fileNameExtensionType = path.extname(req.url);
-
-	let contentType;
-
-	switch (fileNameExtensionType) {
-		case '.css':
-			contentType = 'text/css';
-			break;
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.json':
-			contentType = 'application/json';
-			break;
-		case '.jpg':
-			contentType = 'image/jpeg';
-			break;
-		case '.png':
-			contentType = 'image/png';
-			break;
-		case '.txt':
-			contentType = 'text/plain';
-			break;
-		default:
-			contentType = 'text/html';
-	}
-
-	let filePath =
-		contentType === 'text/html' && req.url === '/'
-			? path.join(__dirname, 'views', 'index.html')
-			: contentType === 'text/html' && req.url.slice(-1) === '/'
-			? path.join(__dirname, 'views', req.url, 'index.html')
-			: contentType === 'text/html'
-			? path.join(__dirname, 'views', req.url)
-			: path.join(__dirname, req.url);
-
-	// makes .html extension not required in the browser
-	if (!fileNameExtensionType && req.url.slice(-1) !== '/') {
-		filePath += '.html';
-	}
-
-	const fileExist = fs.existsSync(filePath);
-
-	if (fileExist) {
-		serveFile(filePath, contentType, res);
-	} else {
-		switch (path.parse(filePath).base) {
-			case 'old-page.html':
-				res.writeHead(301, { Location: '/new-page.html' });
-				res.end();
-				break;
-			case 'www-page.html':
-				res.writeHead(301, { Location: '/' });
-				res.end();
-				break;
-			default:
-				serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-		}
-	}
+// It's a regular expression that matches the root path (`/`) or the path `/index` or
+app.get('^/$|/index(.html)?', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.listen(PORT, () => console.log(`🚀 Server running on PORT ${PORT}`));
+app.get('/new-page(.html)?', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+	res.redirect(301, '/new-page.html');
+});
+
+app.get(
+	'/hello(.html)?',
+	(req, res, next) => {
+		console.log('attempted to load hello.html!');
+
+		next();
+	},
+	(req, res) => {
+		res.send('hello world');
+	}
+);
+
+const one = (req, res, next) => {
+	console.log('one');
+	next();
+};
+
+const two = (req, res, next) => {
+	console.log('two');
+	next();
+};
+
+const three = (req, res, next) => {
+	console.log('finished!');
+	res.send('finished!');
+	next();
+};
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
